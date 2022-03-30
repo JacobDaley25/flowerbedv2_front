c86ce1bb8c8af76f8e269bc78eaadf115550e5c8
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-03-30 12:50:19 -0400

    react router up and running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect,} from 'react'
+import About from './components/About'
+import{Routes,Route, Link} from 'react-router-dom'
+import Profile from './components/Profile'
+import Timeline from './components/Timeline'
+import Login from './components/Login'
+import Register from './components/Register'
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const componentDidMount = ()=>{
+    this.props.history.push('/about')
+  }
+
+  return(<>
+    <h1>hi</h1>
+    <nav>
+      <ul>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/profile'>My Profile</Link></li>
+        <li><Link to='/timeline'>Home</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/register'>Register</Link></li>
+      </ul>
+    </nav>
+
+    <div className='main'>
+      <Routes>
+        <Route path='/about' component={About} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/timeline' component={Timeline} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Routes>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
