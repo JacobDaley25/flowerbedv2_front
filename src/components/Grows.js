a1edb5347a6a0ab4d28ef4b851783cf780a666e6
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-04-02 23:55:08 -0400

    completed mapping on grow and strain components

--- src/components/Grows.js
+++ src/components/Grows.js
@@ -4,13 +4,87 @@ import axios from 'axios'
 const Grows = () => {
   const [grows, setGrows] = useState([])
   const [newName, setNewName] = useState('')
-  const [newLight, setNewLights] = useState('')
+  const [newLight, setNewLight] = useState('')
   const [newLocation, setNewLocation] = useState('')
   const [newImage, setNewImage] = useState('')
   const [newMedium, setNewMedium] = useState('')
   const [newNutrients, setNewNutrients] = useState('')
-  const [newWeek, setNewWeek] = useState('')
+  const [newWeek, setNewWeek] = useState(0)
   const [newHarvestAmount, setNewHarvestAmount] = useState('')
+
+  const handleNewFormSubmit = (event) => {
+    event.preventDefault()
+    axios.post('https://flowerbedv2back.herokuapp.com/grows', {
+      name: newName,
+      light: newLight,
+      indoorOrOutdoor: newLocation,
+      image: newImage,
+      growingMedium: newMedium,
+      nutrientsUsed: newNutrients,
+      week: newWeek,
+      harvestAmount: newHarvestAmount
+    }).then((response)=>{
+      axios.get('https://flowerbedv2back.herokuapp.com/grows')
+      .then((response)=>{
+        setGrows(response.data)
+        setNewName('')
+        setNewLight('')
+        setNewLocation('')
+        setNewImage('')
+        setNewMedium('')
+        setNewNutrients('')
+        setNewWeek(0)
+        setNewHarvestAmount('')
+      })
+    })
+  }
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNewLightChange = (event) => {
+    setNewLight(event.target.value)
+  }
+  const handleNewLocationChange = (event) => {
+    setNewLocation(event.target.value)
+  }
+  const handleNewImageChange = (event) => {
+    setNewImage(event.target.value)
+  }
+  const handleNewMediumChange = (event) => {
+    setNewMedium(event.target.value)
+  }
+  const handleNewNutrientsChange = (event) => {
+    setNewNutrients(event.target.value)
+  }
+  const handleNewWeekChange = (event) => {
+    setNewWeek(event.target.value)
+  }
+  const handleNewHarvestAmount = (event) => {
+    setNewHarvestAmount(event.target.value)
+  }
+  return(
+    <div className='growing-container'>
+    <aside>
+    <h1>Growing Friends</h1>
+    </aside>
+    {
+      grows.map((grow)=>{
+        return <>
+        <div className='growCard' key={grow._id}>
+        <h3 className='textData'>{grow.name}</h3>
+        <img src={grow.image} />
+        <p className='growLights'>{grow.lights}</p>
+        <p className='location'>{grow.indoorOrOutdoor}</p>
+        <p className='growingMedium'>{grow.growingMedium}</p>
+        <p className='nutrientsUsed'>{grow.nutrientsUsed}</p>
+        <p className='week'>{grow.week}</p>
+        <p className='harvest'>{grow.harvestAmount}</p>
+        </div>
+        </>
+      })
+    }
+  </div>
+  )
 }
 
 export default Grows
--- src/components/Strains.js
+++ src/components/Strains.js
@@ -8,6 +8,61 @@ const Strains = () => {
   const [newBody, setNewBody] = useState('')
   const [newImage, setNewImage] = useState('')
 
+  const handleNewFormSubmit = (event) => {
+    event.preventDefault()
+    axios.post('https://flowerbedv2back.herokuapp.com/strains',
+  {
+    name: newName,
+    type: newType,
+    image: newImage,
+    body: newBody
+  }).then((response)=>{
+    axios
+      .get('https://flowerbedv2back.herokuapp.com/strains')
+      .then((response)=>{
+        setStrains(response.data)
+        setNewName('')
+        setNewType('')
+        setNewBody('')
+        setNewImage('')
+      })
+  })
+  }
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNewTypeChange = (event) => {
+    setNewType(event.target.value)
+  }
+  const handleNewBodyChange = (event) => {
+    setNewBody(event.target.value)
+  }
+  const handleNewImageChange = (event) => {
+    setNewImage(event.target.value)
+  }
+
+  return (
+    <div className='strain-container'>
+    <aside>
+    <h1>Strains of the Weed World</h1>
+    </aside>
+    {
+      strains.map((strain)=>{
+        return <>
+        <div className='strainCard' key={strain._id}>
+        <h3 className='textdata'>{strain.name}</h3>
+        <img src={strain.image} />
+        <p className='strainType'>{strain.type}</p>
+        <p className='description'>{strain.body}</p>
+        </div>
+        </>
+  })
+  }
+  </div>)
 }
+
+
+
 
 export default Strains
